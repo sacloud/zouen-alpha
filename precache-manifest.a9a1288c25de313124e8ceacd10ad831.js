5609331f982e82b56208048557a268cbf47f6787
febc.yamamoto <yamamoto.febc@gmail.com>
2020-02-19 18:47:06 +0900

    initial zouen release

---
+++ precache-manifest.a9a1288c25de313124e8ceacd10ad831.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bddd323dcfa13d129a6c83c975e44864",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a242e2569d32c87ef382",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "2066cd0e4bab29497eee",
+    "url": "/static/js/2.ad3bdafa.chunk.js"
+  },
+  {
+    "revision": "581ab1d3cb65f28cfd321e87c3a983e2",
+    "url": "/static/js/2.ad3bdafa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a242e2569d32c87ef382",
+    "url": "/static/js/main.355f72a4.chunk.js"
+  },
+  {
+    "revision": "cfc65071a51c2901e8dc",
+    "url": "/static/js/runtime-main.9ce23339.js"
+  }
+]);
