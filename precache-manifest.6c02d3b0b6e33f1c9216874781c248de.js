68d2d04f65c17059d418203e25a84408f08a1cd0
febc.yamamoto <yamamoto.febc@gmail.com>
2020-04-20 15:35:28 +0900

    terraform-provider-sakuracloud v2.3.0

--- precache-manifest.6c02d3b0b6e33f1c9216874781c248de.js
+++ precache-manifest.6c02d3b0b6e33f1c9216874781c248de.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "e6f6d752b97230532780db2346439fd7",
+    "revision": "6ff16f82a20e3c48aaee9b5580bfc5e7",
     "url": "/index.html"
   },
   {
-    "revision": "847ba47486f320e25c4b",
+    "revision": "e2dfd63bd9c318daa9be",
     "url": "/static/css/main.5ecd60fb.chunk.css"
   },
   {
-    "revision": "bdded31fc19309ffbb55",
-    "url": "/static/js/2.2fba4172.chunk.js"
+    "revision": "af9aca699ec4d792c45e",
+    "url": "/static/js/2.e0c24f69.chunk.js"
   },
   {
     "revision": "581ab1d3cb65f28cfd321e87c3a983e2",
-    "url": "/static/js/2.2fba4172.chunk.js.LICENSE.txt"
+    "url": "/static/js/2.e0c24f69.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "847ba47486f320e25c4b",
-    "url": "/static/js/main.33d3a131.chunk.js"
+    "revision": "e2dfd63bd9c318daa9be",
+    "url": "/static/js/main.aa6696f7.chunk.js"
   },
   {
     "revision": "cfc65071a51c2901e8dc",
